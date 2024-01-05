1c64ac21bed3f57b75e191af9ce1dfb2fa19f936
kulakovskyi <torchok9@gmail.com>
2024-01-05 12:13:49 +0200

    update admin about

--- src/app/about/modules/bio/componets/personal-info/personal-info.component.ts
+++ src/app/about/modules/bio/componets/personal-info/personal-info.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {EditService} from "../../../../../admin/services/edit.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-personal-info',
   templateUrl: './personal-info.component.html',
   styleUrls: ['./personal-info.component.scss']
 })
-export class PersonalInfoComponent {
+export class PersonalInfoComponent implements OnInit, OnDestroy{
+  textAbout!: string
+  aboutTestSub$!: Subscription
+  constructor(private editService: EditService) {
+  }
+  ngOnInit() {
+    this.aboutTestSub$ = this.editService.getUserAboutInfo().subscribe(res => {
+      this.textAbout = res.text
+    })
+  }
+
+  ngOnDestroy() {
+    if(this.aboutTestSub$) this.aboutTestSub$.unsubscribe()
+  }
 
 }
